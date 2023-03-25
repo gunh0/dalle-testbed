cff14f287146c5cede9b4beefbaa071bd8b6e348
Gunho Park <devgun@kakao.com>
2023-03-25 15:37:31 +0900

    dalle pynecone 1st. poc done

--- pynecone/dalle/dalle.py
+++ pynecone/dalle/dalle.py
@@ -1,41 +1,78 @@
 """Welcome to Pynecone! This file outlines the steps to create a basic app."""
 from pcconfig import config
+from dotenv import dotenv_values
 
+import openai
 import pynecone as pc
 
 docs_url = "https://pynecone.io/docs/getting-started/introduction"
 filename = f"{config.app_name}/{config.app_name}.py"
 
+config = dotenv_values("../.env")
+openai.api_key = config["OPENAI_API_KEY"]
+
 
 class State(pc.State):
     """The app state."""
 
-    pass
+    prompt = ""
+    image_url = ""
+    image_processing = False
+    image_made = False
+
+    def process_image(self):
+        """Set the image processing flag to true and indicate that the image has not been made yet."""
+        self.image_made = False
+        self.image_processing = True
+
+    def get_image(self):
+        """Get the image from the prompt."""
+        try:
+            response = openai.Image.create(prompt=self.prompt, n=1, size="256x256")
+            self.image_url = response["data"][0]["url"]
+            # Set the image processing flag to false and indicate that the image has been made.
+            self.image_processing = False
+            self.image_made = True
+        except:
+            self.image_processing = False
+            return pc.window_alert("Error with OpenAI Execution.")
 
 
 def index():
     return pc.center(
         pc.vstack(
-            pc.heading("Welcome to Pynecone!", font_size="2em"),
-            pc.box("Get started by editing ", pc.code(filename, font_size="1em")),
-            pc.link(
-                "Check out our docs!",
-                href=docs_url,
-                border="0.1em solid",
-                padding="0.5em",
-                border_radius="0.5em",
-                _hover={
-                    "color": "rgb(107,99,246)",
-                },
+            pc.heading("DALL-E", font_size="1.5em"),
+            pc.input(placeholder="Enter a prompt..", on_blur=State.set_prompt),
+            pc.button(
+                "Generate Image",
+                on_click=[State.process_image, State.get_image],
+                width="100%",
             ),
-            spacing="1.5em",
-            font_size="2em",
+            pc.divider(),
+            pc.cond(
+                State.image_processing,
+                pc.circular_progress(is_indeterminate=True),
+                pc.cond(
+                    State.image_made,
+                    pc.image(
+                        src=State.image_url,
+                        height="25em",
+                        width="25em",
+                    ),
+                ),
+            ),
+            bg="white",
+            padding="2em",
+            shadow="lg",
+            border_radius="lg",
         ),
-        padding_top="10%",
+        width="100%",
+        height="100vh",
+        background="radial-gradient(circle at 22% 11%,rgba(62, 180, 137,.20),hsla(0,0%,100%,0) 19%),radial-gradient(circle at 82% 25%,rgba(33,150,243,.18),hsla(0,0%,100%,0) 35%),radial-gradient(circle at 25% 61%,rgba(250, 128, 114, .28),hsla(0,0%,100%,0) 55%)",
     )
 
 
 # Add state and page to the app.
 app = pc.App(state=State)
-app.add_page(index)
+app.add_page(index, title="Pynecone:DALL-E")
 app.compile()
